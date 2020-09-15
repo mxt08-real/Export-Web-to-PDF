0fab695df6a1e744cbca0f56df528da1a253b298
mxt08-real <59659815+mxt08-real@users.noreply.github.com>
2020-09-15 17:14:29 +0200

    Add files via upload

--- background.js
+++ background.js
@@ -4,4 +4,18 @@ browser.browserAction.setIcon({path: "check.png"});
 browser.browserAction.setTitle({title: "saved"}); 
 setTimeout(function() {
 browser.browserAction.setIcon({path: "icon.png"});
-browser.browserAction.setTitle({title: "Export Web to PDF"}); },5000); });
+browser.browserAction.setTitle({title: "Export Web to PDF"}); },10000); });
+function a(b) {
+if(b.length < 0) {
+var c = browser.downloads.open(b[0].id);
+c.then(d, e); } }
+function d() {
+console.log("Opened correctly"); }
+function e(f) {
+console.log("Error opening file"); }
+var g = browser.downloads.search({
+  limit: 1,
+  orderBy: ["-startTime"]
+});
+g.then(c, f);
+
